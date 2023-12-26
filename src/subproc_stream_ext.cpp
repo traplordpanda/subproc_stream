b2419fd34c295fcce7c32a6a8096eff438bc99f5
aubuchon-kyle <aubuchon_kyle@bah.com>
2023-12-26 14:12:24 -0500

    Fixing recursive submodule checkout

--- src/subproc_stream_ext.cpp
+++ src/subproc_stream_ext.cpp
@@ -1,8 +1,8 @@
-#include <string_view>
 #include <iostream>
 #include <nanobind/nanobind.h>
-#include <nanobind/stl/string_view.h>
 #include <nanobind/stl/filesystem.h>
+#include <nanobind/stl/string_view.h>
+#include <string_view>
 
 #include "subproc_stream.hpp"
 
@@ -11,10 +11,9 @@ namespace nb = nanobind;
 NB_MODULE(subproc_stream_ext, m) {
     using namespace subproc_stream;
     nb::class_<SubProc>(m, "SubProc")
-        .def(nb::init<const std::string_view>(), nb::rv_policy::copy)
+        .def(nb::init<const std::string_view>(), nb::rv_policy::take_ownership)
         .def("get_return_code", &SubProc::get_return_code)
         .def("exec", &SubProc::exec)
-        .def("print_test", []{
-        std::cout << "Hello from subproc_stream_ext.cpp\n";
-    });
+        .def("print_test",
+             [] { std::cout << "Hello from subproc_stream_ext.cpp\n"; });
 }
